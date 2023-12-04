a1f14ed6e2c2c36a0454e2d8c4feeb198a7b969f
Karmick <u6270742@anu.edu.au>
2023-12-05 00:30:01 +1100

    advent of code 2023 day 1 and 2

---
+++ src/day1.rs
@@ -0,0 +1,52 @@
+use std::{fs, collections::HashMap};
+use fancy_regex::Regex;
+
+fn main() {
+    let file_path = "./input/day1.txt";
+    
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let mut summed: u32 = 0;
+
+    for raw_line in contents.lines() {
+        println!("{} is the original string", raw_line);
+        
+        let re = Regex::new("(?=(one|two|three|four|five|six|seven|eight|nine|\\d))").unwrap();
+        let mut captures = re.captures_iter(raw_line);
+
+        let first = captures.nth(0).unwrap().unwrap().get(1).unwrap().as_str();
+        summed = summed + add_digit(first) * 10;
+
+        let last : &str;
+        if let Some(next) = captures.last() {
+            last = next.unwrap().get(1).unwrap().as_str();
+        } else {
+            last = &first;
+        }
+        summed = summed + add_digit(last);
+
+        println!("({} * 10 + {}) needs to be added", first, last);
+        println!("{}", summed);
+    }
+}
+
+fn add_digit(s: &str) -> u32 {
+    let digit_words: HashMap<&str, u32> = HashMap::from([
+        ("one",     1),
+        ("two",     2), 
+        ("three",   3), 
+        ("four",    4), 
+        ("five",    5), 
+        ("six",     6), 
+        ("seven",   7), 
+        ("eight",   8), 
+        ("nine",    9)
+    ]);
+
+    if let Some(d) = digit_words.get(s) {
+        *d
+    } else {
+        s.parse::<u32>().unwrap()
+    }
+} 
---
+++ src/day2.rs
@@ -0,0 +1,57 @@
+use std::{fs, collections::HashMap};
+use fancy_regex::Regex;
+
+fn main() {
+    let file_path = "./input/day2.txt";
+    
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let mut games:Vec<u32> = Vec::new(); 
+
+    let mut powers:Vec<u32> = Vec::new();
+
+    for raw_line in contents.lines() {
+        let game_num = raw_line.split(": ").next().unwrap().split(" ").nth(1).unwrap().parse::<u32>().unwrap();
+
+        let sets = raw_line.split(": ").nth(1).unwrap();
+
+        let mut max_cube:HashMap<&str, u32> = HashMap::from([
+            ("red",   0),
+            ("green", 0), 
+            ("blue",  0)
+        ]);
+
+        for set in sets.split("; ") {
+            let re = Regex::new("(((?:1[3-9]|[2-9]\\d) red)|((?:1[4-9]|[2-9]\\d) green)|((?:1[5-9]|[2-9]\\d) blue))").unwrap();
+            if let Some(capture) = re.captures_iter(set).next() {
+                if !(games.contains(&game_num)) {
+                    games.push(game_num);
+                }
+            }
+
+            for cubes in set.split(", ") {
+                let cube_num = cubes.split(" ").next().unwrap().parse::<u32>().unwrap();
+                for colour in ["red", "green", "blue"] {
+                    if cubes.contains(colour) && (cube_num > *max_cube.get(colour).unwrap()) {
+                        max_cube.insert(colour, cube_num);
+                    }
+                }
+            }
+        }
+        
+        let mut power:u32 = 1; 
+        for (k, v) in max_cube {
+            power = power * v;
+        }
+        powers.push(power);
+    }
+
+    // Part 1: I misread the question and got the cheating games instead. Fixed it by some quick math hack :P
+    let cheated: u32 = games.iter().sum();
+    println!("{:?}", 101*100/2 - cheated);
+
+    // Part 2
+    let power_sum: u32 = powers.iter().sum();
+    println!("{}", power_sum);
+}
